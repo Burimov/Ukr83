9498c9269c50ede8050391c237547b6a9798f94a
Burimov <denysburimov@gmail.com>
2018-01-17 16:27:24 +0200

    Принято тестировщиком

--- Ukr8300.py
+++ Ukr8300.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def bouquets(narcissus_price, tulip_price, rose_price, summ):
-
-
     def amount_of_single_type(cash, flowers_price):
         check_summ = 0
         flowers_amount = 0
@@ -17,7 +15,6 @@ def bouquets(narcissus_price, tulip_price, rose_price, summ):
 
         return flowers_amount
 
-
     narcissus_amount = amount_of_single_type(summ, narcissus_price)
     tulip_amount = amount_of_single_type(summ, tulip_price)
     rose_amount = amount_of_single_type(summ, rose_price)
@@ -26,11 +23,10 @@ def bouquets(narcissus_price, tulip_price, rose_price, summ):
     for i in range(narcissus_amount + 1):
         for j in range(tulip_amount + 1):
             for k in range(rose_amount + 1):
-                if (narcissus_price*i + tulip_price*j + rose_price*k) <= summ and (i + j + k) != 0 and (i+ j + k)%2 !=0:
+                if (narcissus_price * i + tulip_price * j + rose_price * k) <= summ and (i + j + k) != 0 and (
+                        i + j + k) % 2 != 0:
                     # print ('n'*i + 't'*j + 'r'*k), (str(i) + ' n ' + str(j) + ' t ' + str(k) + ' r '), (narcissus_price*i + tulip_price*j + rose_price*k), summ
                     total_amount += 1
-
-
 
     return total_amount
 
